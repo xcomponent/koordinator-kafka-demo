bb72206c36b77fbf33990eaa2b1c25e01ace933c
Marcos ALMEIDA <marcos.almeida@xcomponent.com>
2018-10-15 17:25:28 +0200

    ci: change order, sleep

--- download-image/test.sh
+++ download-image/test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 docker run -d --rm -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=localhost --env ADVERTISED_PORT=9092 --env AUTO_CREATE_TOPICS=true spotify/kafka
-./run-producer.sh urls
-./run-consumer.sh urls
+echo Waiting...
+sleep 30
+echo Running consumer...
+./run-consumer.sh urls12 &
+
+echo Running producer...
+./run-producer.sh urls12 &
