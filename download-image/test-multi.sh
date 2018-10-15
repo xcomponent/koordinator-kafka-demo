26b3c044325203f5d29b3f871e4fc539abaace3c
Marcos ALMEIDA <marcos.almeida@xcomponent.com>
2018-10-15 18:46:53 +0200

    ci: run simple version

--- download-image/test-multi.sh
+++ download-image/test-multi.sh
@@ -6,17 +6,28 @@ KAFKA=$(docker run -d --rm -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=local
 echo Kafka container $KAFKA started...
 sleep 30
 
-echo Running consumer...
+echo Creating topics..
+~/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 6 --topic $TOPIC
+
+echo Running consumers...
 ./run-consumer.sh $TOPIC & 
 P1=$!
 echo Started consumer PID=$P1!
 
+./run-consumer.sh $TOPIC & 
+P2=$!
+echo Started consumer PID=$P2!
+
+./run-consumer.sh $TOPIC & 
+P3=$!
+echo Started consumer PID=$P3!
+
 sleep 30
 echo Running producer...
 ./run-producer.sh $TOPIC &
-P2=$!
-echo Started producer PID=$P2!
+P4=$!
+echo Started producer PID=$P4!
 
-wait $P1 $P2
+wait $P1 $P2 $P3 $P4
 
 docker stop $KAFKA
