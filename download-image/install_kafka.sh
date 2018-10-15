09e065aaa366a3d2a6a9dd510cd6590627276d2c
Marcos ALMEIDA <marcos.almeida@xcomponent.com>
2018-10-15 18:48:03 +0200

    ci: missing files

--- download-image/install_kafka.sh
+++ download-image/install_kafka.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl "http://www-eu.apache.org/dist/kafka/1.1.0/kafka_2.12-1.1.0.tgz" -o ~/kafka.tgz &&
-mkdir ~/kafka &&
-cd ~/kafka &&
+curl "http://www-eu.apache.org/dist/kafka/1.1.0/kafka_2.12-1.1.0.tgz" -o ~/kafka.tgz
+mkdir ~/kafka
+cd ~/kafka
 tar -xvzf ~/kafka.tgz --strip 1
 
