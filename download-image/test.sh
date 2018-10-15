ebf31bdbf255ca25bd0f331b698f4a884982bee0
Marcos ALMEIDA <marcos.almeida@xcomponent.com>
2018-10-15 20:43:11 +0200

    ci: run kafka separatedly

--- download-image/test.sh
+++ download-image/test.sh
@@ -3,11 +3,6 @@
 BROKER=$1
 TOPIC=urls
 
-docker run --name kafka -d --rm -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=localhost --env ADVERTISED_PORT=9092 --env AUTO_CREATE_TOPICS=true spotify/kafka
-
-echo Kafka container $KAFKA started...
-sleep 30
-
 echo Running consumer...
 ./run-consumer.sh $TOPIC $BROKER & 
 P1=$!
@@ -20,5 +15,3 @@ P2=$!
 echo Started producer PID=$P2!
 
 wait $P1 $P2
-
-docker stop kafka
