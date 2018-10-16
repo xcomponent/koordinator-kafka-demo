3d3df535b76dcda6ef4d6c8d017705a2b8f5783e
Marcos ALMEIDA <marcos.almeida@xcomponent.com>
2018-10-16 08:42:51 +0200

    test: fix multi consumer

--- download-image/test-multi.sh
+++ download-image/test-multi.sh
@@ -2,34 +2,30 @@
 set -o errexit
 set -o nounset
 
-TOPIC=urls
-KAFKA=$(docker run -d --rm -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=localhost --env ADVERTISED_PORT=9092 --env AUTO_CREATE_TOPICS=true spotify/kafka)$
-
-echo Kafka container $KAFKA started...
-sleep 30
+BROKER=$1
+TOPIC=urls18
+LIMIT=20
 
 echo Creating topics..
-~/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 6 --topic $TOPIC
+~/kafka/bin/kafka-topics.sh --create --zookeeper $BROKER:2181 --replication-factor 1 --partitions 6 --topic $TOPIC
 
 echo Running consumers...
-./run-consumer.sh $TOPIC & 
+./run-consumer.sh $TOPIC $BROKER & 
 P1=$!
 echo Started consumer PID=$P1!
 
-./run-consumer.sh $TOPIC & 
+./run-consumer.sh $TOPIC $BROKER & 
 P2=$!
 echo Started consumer PID=$P2!
 
-./run-consumer.sh $TOPIC & 
+./run-consumer.sh $TOPIC $BROKER & 
 P3=$!
 echo Started consumer PID=$P3!
 
 sleep 30
 echo Running producer...
-./run-producer.sh $TOPIC &
+./run-producer.sh $TOPIC $BROKER $LIMIT &
 P4=$!
 echo Started producer PID=$P4!
 
 wait $P1 $P2 $P3 $P4
-
-docker stop $KAFKA
