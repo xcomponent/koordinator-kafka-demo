fab0847b467fbcdf6d59500b0f28c01d01b337ce
Marcos ALMEIDA <marcos.almeida@xcomponent.com>
2018-10-17 10:37:30 +0200

    sh: remove install_kafka script

--- .circleci/test-multi.sh
+++ .circleci/test-multi.sh
@@ -3,7 +3,10 @@ set -o errexit
 set -o nounset
 
 echo Installing Kafka tools...
-/src/project/install_kafka.sh
+curl "http://www-eu.apache.org/dist/kafka/1.1.0/kafka_2.12-1.1.0.tgz" -o ~/kafka.tgz
+mkdir ~/kafka
+cd ~/kafka
+tar -xvzf ~/kafka.tgz --strip 1
 
 cd /src/project/download-image
 bash ./test-multi.sh kafka 12 urls ~/kafka
