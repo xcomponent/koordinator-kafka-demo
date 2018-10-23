d701258295b7ee2548d1b76d134e0a2469dd80c5
Marcos ALMEIDA <marcos.almeida@xcomponent.com>
2018-10-23 13:49:25 +0200

    consumer: read from beginning

--- download-image/src/main/java/example/Kafka.java
+++ download-image/src/main/java/example/Kafka.java
@@ -18,6 +18,7 @@ public class Kafka {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
 
         consumer.subscribe(Arrays.asList(topicToRead)); 
